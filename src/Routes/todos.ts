dc69585371c944cc19e1da2b0f533c0b89341e73
RIYA VERMA <riyavmt14@gmail.com>
2024-01-28 14:22:36 +0530

    Added dist and src folders

--- src/Routes/todos.ts
+++ src/Routes/todos.ts
@@ -4,7 +4,7 @@ const router = Router();
 let todos : todo[] =[];
 
 router.get('/',(req,res,next)=>{
-    res.status(200).json({todos:todos})
+    res.status(200).json({todos:todos});
 });
 router.post('/todo',(req,res,next)=>{
     const newTodo : todo = {
@@ -12,20 +12,20 @@ router.post('/todo',(req,res,next)=>{
         text: req.body.text
     }
     todos.push(newTodo);
-    res.status(201).json({message:"Added Todo",todo:newTodo, todos:todos})
+    res.status(201).json({message:"Added Todo",todo:newTodo, todos:todos});
 });
 router.put('/edit/:id',(req,res,next)=>{
     const todoId = req.params.id;
     const index = todos.findIndex(element=>element.id === todoId);
     if(index>=0){
         todos[index] = { id:todos[index].id, text : req.body.text};
-        return res.status(200).json({message:"Updated the todo.",todos:todos})
+        return res.status(200).json({message:"Updated the todo.",todos:todos});
     }
-    res.status(404).json({message:"Error"})
+    res.status(404).json({message:"Error"});
 
 });
 router.delete('/delete/:id',(req,res,next)=>{
     todos = todos.filter((element)=>element.id !== req.params.id);
-    return res.status(200).json({message:"Deleted the todo!"})
+    return res.status(200).json({message:"Deleted the todo!"});
 })
 export default router;
--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import todosRoutes from './Routes/todos'
+import todosRoutes from './Routes/todos';
+
 const app = express();
+
 app.use(bodyParser.json());
- app.use(todosRoutes);
+app.use(todosRoutes);
+
 app.listen(3000);
